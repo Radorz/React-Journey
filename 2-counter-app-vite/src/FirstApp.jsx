12bacab88c6163e204385f3cab813b50a9ebe8f1
Luis Franco <luisin.francof@gmail.com>
2025-02-18 21:30:14 -0400

    Avances props components

---
+++ 2-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FirstApp = ({title, subTitle}) => {
+  return (
+    <>
+    <h1>{title} {1+5}</h1>
+    <p>{subTitle}</p>      
+    </>
+  )
+}
+
+export default FirstApp
--- 2-counter-app-vite/src/main.jsx
+++ 2-counter-app-vite/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {App} from "./HelloWorldApp";
-
+import FirstApp from "./FirstApp";
+import "./styles.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
 <App />
+<FirstApp title="Hola pajitatox" subTitle="sung jiwo"/>
 </React.StrictMode>
 );
+
+
