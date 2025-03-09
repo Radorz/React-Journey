0510207c838d112354d8dbbd131931c5bec3100e
Luis Franco <luisin.francof@gmail.com>
2025-03-09 01:25:54 -0400

    test

--- 2-counter-app-vite/src/main.jsx
+++ 2-counter-app-vite/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import {App} from "./HelloWorldApp";
 import FirstApp from "./FirstApp";
 import "./styles.css";
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-<App />
-<FirstApp title="Hola pajitatox" subTitle="sung jiwo"/>
+    <FirstApp title={true} />
 </React.StrictMode>
-);
-
+  );
 
