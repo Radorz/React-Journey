086d22b5db71b86b2fdce2ff0b16e99a00a1f9b8
Luis Franco <luisin.francof@gmail.com>
2025-08-16 02:27:02 -0400

    Initialize Vite + TypeScript project
    Added initial project files for a Vite + TypeScript setup, including configuration, source files, and assets. Removed unnecessary __MACOSX artifacts.

---
+++ 01-intro/src/bases/01-const-let.ts
@@ -0,0 +1,13 @@
+
+
+
+const firstName : string = 'Luis';
+const lastName = 'Franco';
+const containLetterL = lastName.includes('F');
+
+console.log(firstName, lastName);
+
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log({containLetterL, diceNumber});
