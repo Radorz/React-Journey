c3ad751bcdc72b263af7dd4741e43cda26079b05
Luis Franco <luisin.francof@gmail.com>
2025-08-18 17:12:27 -0400

    Add functions example and update imports
    Added 05-functions.ts with examples of function declarations and arrow functions. Updated main.ts to import the new functions file instead of arrays. Also added array cloning examples to 04-arrays.ts.

---
+++ 01-intro/src/bases/04-arrays.ts
@@ -0,0 +1,8 @@
+const myArray: number[] = [1,2,3,4,5,6];
+
+const myArray2 = structuredClone(myArray);
+const myArray3 = [...myArray];
+
+myArray2.push(7);
+
+console.log({myArray, myArray2})
---
+++ 01-intro/src/bases/05-functions.ts
@@ -0,0 +1,33 @@
+
+
+function greet(name: string) : string {
+    return `Hola ${name}`;
+}
+
+
+const greet2 = (name: string): string => {
+    return `Hola ${name}`;
+}
+
+const message = greet('Hola');
+const message2 = greet('Hola');
+console.log(message, message2);
+
+function getUser() {
+    return{
+        uid: 'ABC-123',
+        username: 'Pajita tox'
+    }
+}
+
+
+const getUser2 = () =>{
+     return{
+        uid: 'ABC-123',
+        username: 'Pajita tox'
+    }
+}
+
+const user = getUser();
+const user2 = getUser2();
+console.log(user, user2);
